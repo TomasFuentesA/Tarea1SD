e0c3bf875d7cbed2fcdcd579765c1c1219fa8092
TomasFuentesA <tomas.fuentes@mail.udp.cl>
2022-04-22 17:41:34 -0400

    server + conexion db

--- Servidor/server_grpc.py
+++ Servidor/server_grpc.py
@@ -2,6 +2,7 @@ import grpc
 from concurrent import futures
 import proto_message_pb2
 import proto_message_pb2_grpc
+from psycopg2 import connect
 
 class buscar(proto_message_pb2_grpc.ItemServiceServicer):
 
@@ -11,6 +12,9 @@ class buscar(proto_message_pb2_grpc.ItemServiceServicer):
     def GetServerResponse(self, request, context):
         print ("Hola mundo")
 
+def db_con():
+    conn = connect(host="postgres" ,dbname="Tarea1", user="postgres", password="postgres", port= 5432)
+    return conn
 
 def serve():
 
@@ -18,8 +22,18 @@ def serve():
     proto_message_pb2_grpc.add_ItemServiceServicer_to_server(buscar(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
+    conn = db_con()
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM Items;")
+    for i, record in enumerate(cursor):
+        print ("\n", type(record))
+        print ( record )
+
+    # close the cursor object to avoid memory leaks
+    cursor.close()
+
     server.wait_for_termination()
 
 
 if __name__ == '__main__':
-    serve()
+    serve() 
