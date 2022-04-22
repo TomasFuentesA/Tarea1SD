a47a7072bd2b214c0efa7b803bcd2f9e1e394f8e
TomasFuentesA <tomas.fuentes@mail.udp.cl>
2022-04-22 13:57:16 -0400

    Server corriendo

--- Servidor/server_grpc.py
+++ Servidor/server_grpc.py
@@ -5,6 +5,9 @@ import proto_message_pb2_grpc
 
 class buscar(proto_message_pb2_grpc.ItemServiceServicer):
 
+    def __init__(self):
+        print("Inicialización correcta")
+
     def GetServerResponse(self, request, context):
         print ("Hola mundo")
 
@@ -15,5 +18,8 @@ def serve():
     proto_message_pb2_grpc.add_ItemServiceServicer_to_server(buscar(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
+    server.wait_for_termination()
 
-    return "Hola"
+
+if __name__ == '__main__':
+    serve()
